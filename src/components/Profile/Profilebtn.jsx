c57a161725b66353bb36d062492b04eed0192762
Ravi Jaiswal <ravijaiswal258852@gmail.com>
2024-09-19 17:52:41 +0530

    logout function updated

--- src/components/Profile/Profilebtn.jsx
+++ src/components/Profile/Profilebtn.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 import {
-  DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { signOut } from "next-auth/react";
 import { User, LogOut, MapPinHouse, Truck } from "lucide-react";
 import Link from "next/link";
+import { useToast } from "../ui/use-toast";
 
 const Profilebtn = () => {
+  const { toast } = useToast();
+  const handleSignOut = async () => {
+    try {
+
+      await signOut({ redirect: false });
+      toast({
+        title: "Signed Out",
+        description: "You have successfully signed out.",
+        status: "success",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "An error occurred during sign out.",
+        status: "error",
+      });
+    }
+  };
 
   const menuData = [
     {
       name: "Profile",
-      icon:<User className="mr-2 h-4 w-4" />,
+      icon: <User className="mr-2 h-4 w-4" />,
       url: "",
     },
     {
       name: "Address",
-      icon:<MapPinHouse className="mr-2 h-4 w-4" />,
+      icon: <MapPinHouse className="mr-2 h-4 w-4" />,
       url: "",
     },
     {
       name: "Track Order",
-      icon:<Truck className="mr-2 h-4 w-4" />,
+      icon: <Truck className="mr-2 h-4 w-4" />,
       url: "",
     },
   ];
@@ -43,7 +60,7 @@ const Profilebtn = () => {
         </Link>
       ))}
       <DropdownMenuSeparator />
-      <DropdownMenuItem onClick={() => signOut()}>
+      <DropdownMenuItem  onClick={() => handleSignOut()} >
         <LogOut className="mr-2 h-4 w-4" />
         <span>Log out</span>
       </DropdownMenuItem>
